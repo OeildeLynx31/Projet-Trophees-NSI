27bc53fe116fc4ea8e152c1b97d96ac3dad14000
OeildeLynx31 <vincent@top7box.com>
2025-10-09 18:41:21 +0200

    feat(stage): add ticking for each sprites of a stage

--- app/stage1/stage.py
+++ app/stage1/stage.py
@@ -3,13 +3,17 @@ from ..player import Player
 
 class Stage():
     def __init__(self, game):
+        self.game = game
         self.group = pygame.sprite.Group()
+
         self.player = Player()
         self.player.add(self.group)
-        self.game = game
 
     def tick(self):
         #L'arrière-plan futur
         #world.blit(backdrop, backdropbox)
         self.group.draw(self.game)
         pygame.display.flip()
+
+        for sprite in self.group.sprites():
+            sprite.tick() #run the tick method for each sprite in the stage
