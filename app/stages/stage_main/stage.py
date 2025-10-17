3a0c1991d4fb2a838275598322fe91dff589aa57
Jchoq31 <jchoq31@outlook.com>
2025-10-17 19:48:24 +0000

    feat(main_menu):Adding news button (settings doesn't have image after clicking)

--- app/stages/stage_main/stage.py
+++ app/stages/stage_main/stage.py
@@ -10,17 +10,21 @@ class Stage():
     self.group = pygame.sprite.Group()
     self.backdrop = pygame.transform.scale(pygame.image.load(os.path.join('./assets/backgrounds/', "bg_interface__main.png")), (1280, 720)).convert_alpha()
 
-    self.play_button = Button(540, 250, 200, 200, "main_play.png")
-    self.quit_button = Button(540, 320, 200, 200, "main_quit.png")
+    self.play_button = Button(535, 300, 208, 93, "main_play.png")
+    self.quit_button = Button(535, 400, 208, 93, "main_quit.png")
+    self.settings_button = Button(535, 350, 208, 93, "main_settings.png")
 
   def tick(self, game):
     self.game = game
     self.screen.blit(self.backdrop, (0, 0))
     self.play_button.draw(self.screen)
     self.quit_button.draw(self.screen)
+    self.settings_button.draw(self.screen)
     self.group.draw(self.screen)
     if (self.play_button.isClicked()):
         self.game.changeStage("1")
+    if (self.settings_button.isClicked()):
+        self.game.changeStage("settings")
     if (self.quit_button.isClicked()):
         pygame.quit()
         exit()
