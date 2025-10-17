c18672e5e782df6c851e344882c08ca99e930caa
Jchoq31 <jchoq31@outlook.com>
2025-10-17 19:42:23 +0200

    feat(main_menu): Finishing adding the quit button

--- app/stages/stage_main/stage.py
+++ app/stages/stage_main/stage.py
@@ -11,12 +11,13 @@ class Stage():
     self.backdrop = pygame.transform.scale(pygame.image.load(os.path.join('./assets/backgrounds/', "bg_interface__main.png")), (1280, 720)).convert_alpha()
 
     self.play_button = Button(540, 250, 200, 200, "main_play.png")
-    self.quit_button = Button(540, 470, 200, 200, "main_quit.png")
+    self.quit_button = Button(540, 320, 200, 200, "main_quit.png")
 
   def tick(self, game):
     self.game = game
     self.screen.blit(self.backdrop, (0, 0))
     self.play_button.draw(self.screen)
+    self.quit_button.draw(self.screen)
     self.group.draw(self.screen)
     if (self.play_button.isClicked()):
         self.game.changeStage("1")
