24327ea311aed80b04b432ba8f5f1753b8927e8f
OeildeLynx31 <vincent@top7box.com>
2025-10-27 12:24:05 +0100

    feat(main screen): add game title

--- app/stages/stage_main/stage.py
+++ app/stages/stage_main/stage.py
@@ -8,15 +8,17 @@ class Stage():
     self.game = game
     self.screen = game.screen
     self.group = pygame.sprite.Group()
-    self.backdrop = pygame.transform.scale(pygame.image.load(os.path.join('./assets/backgrounds/', "bg_interface__main.png")), (1280, 720)).convert_alpha()
+    self.backdrop = pygame.transform.scale(pygame.image.load(os.path.join('./assets/backgrounds/', "bg_interface__main.png")), (1280, 720)).convert()
 
     self.play_button = Button(512, 288, 256, 128, "main_play.png")
     self.quit_button = Button(512, 384, 256, 128, "main_quit.png")
     self.settings_button = Button(512, 480, 256, 128, "main_settings.png")
+    self.title = pygame.transform.scale(pygame.image.load(os.path.join('./assets/interface/', "title.png")), (876, 248)).convert_alpha()
 
   def tick(self, game):
     self.game = game
     self.screen.blit(self.backdrop, (0, 0))
+    self.screen.blit(self.title, (202, 48))
     self.play_button.draw(self.screen)
     self.quit_button.draw(self.screen)
     self.settings_button.draw(self.screen)
