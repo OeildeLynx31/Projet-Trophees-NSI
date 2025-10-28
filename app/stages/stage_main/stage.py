e5dbb35b447528b0cef3ed14cf3b486abf0cda17
OeildeLynx31 <vincent@top7box.com>
2025-10-28 18:59:03 +0100

    fix(main stage): exchange "quit" and "settings" buttons positions

--- app/stages/stage_main/stage.py
+++ app/stages/stage_main/stage.py
@@ -11,8 +11,8 @@ class Stage():
     self.backdrop = pygame.transform.scale(pygame.image.load(os.path.join('./assets/backgrounds/', "bg_interface__main.png")), (1280, 720)).convert()
 
     self.play_button = Button(512, 288, 256, 128, "main_play.png")
-    self.quit_button = Button(512, 384, 256, 128, "main_quit.png")
-    self.settings_button = Button(512, 480, 256, 128, "main_settings.png")
+    self.quit_button = Button(512, 480, 256, 128, "main_quit.png")
+    self.settings_button = Button(512, 384, 256, 128, "main_settings.png")
     self.title = pygame.transform.scale(pygame.image.load(os.path.join('./assets/interface/', "title.png")), (876, 248)).convert_alpha()
 
   def tick(self, game):
